2bae9f6fc6e79d24fdc0a8c4d5b91d9c44f50d11
KevinNuriawan <120350473+KevinNuriawan@users.noreply.github.com>
2025-01-22 15:10:54 +0700

    menambahkan file navbar

---
+++ navbar.php
@@ -0,0 +1,34 @@
+<nav class="navbar navbar-expand-lg bg-primary-subtle">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">SIM</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/UAS-Sri-maharani/index.php">Beranda</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/UAS-Sri-maharani/modul-karyawan/index.php">Data Karyawan</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/UAS-Sri-maharani/modul-divisi">Data Divisi</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/project/modul-dosen/index.php">Data Dosen</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/project/ubah-password.php">Ubah Password</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/project/logout.php">Logout</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
